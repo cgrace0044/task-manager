8898186a6d676c36c3e9d140d8c3e237709f7dcc
cgrace0044 <cgrace0044@gmail.com>
2025-04-21 15:37:48 +0200

    remove copy paste error

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -13,9 +13,6 @@ db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
